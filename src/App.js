1a938c3ac8aa4f33eb76620db0f9d65ae31265aa
Vaibhavi <porevaibhavi5@gmail.com>
2022-01-07 16:26:06 +0530

    updated Grid

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
-import Header from './Components/Header';
-import Button from './Components/Button';
-import Grid from './Components/Grid';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import Header from "./Components/Header";
+import Button from "./Components/Button";
+import Grid from "./Components/Grid";
+import { useEffect, useState } from "react";
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
@@ -13,44 +13,54 @@ function App() {
   const fetchData = async () => {
     setIsLoading(true);
     setError(null);
-    try{
-      const response = await fetch('https://crossworddb-default-rtdb.firebaseio.com/WordData.json');
+    try {
+      const response = await fetch(
+        "https://crossworddb-default-rtdb.firebaseio.com/WordData.json"
+      );
 
-      if(!response.ok){
-        throw new Error('Could not fetch Data!');
+      if (!response.ok) {
+        throw new Error("Could not fetch Data!");
       }
 
       const data = await response.json();
 
-      for(const key in data){
-        loadedData.push({id: key, word: data[key].word, clue: data[key].clue});
+      for (const key in data) {
+        loadedData.push({
+          id: key,
+          word: data[key].word,
+          clue: data[key].clue,
+        });
       }
-      
+
       setPairs(loadedData);
-    }
-    catch(error){
-      setError(error.message || 'Something went Wrong!');
+    } catch (error) {
+      setError(error.message || "Something went Wrong!");
     }
     setIsLoading(false);
+  };
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+  if (pairs.length > 0) {
+    var words = pairs.map((eachWord) => eachWord.word);
+
+    var clues = pairs.map((eachClue) => eachClue.clue);
   }
 
-  useEffect( () => {
-    fetchData();
-  }, []);
-if(pairs.length > 0){
-  var words = pairs.map((eachWord) => (
-    eachWord.word
-  ));
-
-  var clues = pairs.map((eachClue) => (
-    eachClue.clue
-  ));
-}
   return (
     <>
-      <Header/>
-      <Button onFetch={fetchData}/>
-      <Grid words={words} clues={clues} loading={isLoading} error={error} onFetch={fetchData}/>
+      <Header />
+      <Button onFetch={fetchData} />
+      <Grid
+        pairs={pairs}
+        words={words}
+        clues={clues}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchData}
+      />
     </>
   );
 }
--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -1,62 +1,116 @@
 import "../Assets/Grid.css";
 const Grid = (props) => {
 
-  var reqClues=[];
+  let grids = [];
+  let wordSet = [];
+  let clueSet = [];
+  let reqClues = [];
+  let reqWords = [];
+  let usedWords = [];
+  let firstLetters = [];
+  let index = [];
+  let word = {text: '', row: 0, column: 0, vertical: true};
 
-  const wordsNClues = (words, clues) => {
-    var reqWords = getRandomData(words, 5);
-    var index = [];
+  const getSpecWord = () => {
+    let specWords = wordSet.filter((eachWord) => eachWord.length>=6);
+    return specWords[Math.floor(Math.random() * specWords.length)];
+  };
 
+  const pushUsedWords = (text) => {
+    usedWords.push(text);
+    text.split("").filter((eachLetter) => firstLetters.push(eachLetter));
+    console.log(firstLetters);
+  };
+
+  const generateGrids = () => {
+    grids = [];
+    for(let gridsMade = 0; gridsMade<1; gridsMade++){
+      word.text = getSpecWord();
+      word.row = 0;
+      word.column = 0;
+      word.vertical = false;
+      
+      pushUsedWords(word.text);
+    }
+  };
+
+  const obtainClues = () => {
     reqWords.forEach((eachReqWord) =>
-      words.forEach((eachWord) => {
+      wordSet.forEach((eachWord) => {
         if (eachReqWord === eachWord) {
-          index.push(words.indexOf(eachWord));
+          index.push(wordSet.indexOf(eachWord));
         }
       })
     );
 
     index.forEach((eachIndex) =>
-      clues.forEach((eachClue) => {
-        if (eachIndex === clues.indexOf(eachClue)) {
+      clueSet.forEach((eachClue) => {
+        if (eachIndex === clueSet.indexOf(eachClue)) {
           reqClues.push(`${index.indexOf(eachIndex)+1}. ${eachClue}`);
           reqClues.push(<br/>);
         }
       })
     );
-
-    console.log(reqWords);
-    console.log(reqClues);
   };
 
-  const getRandomData = (arr, n) => {
-    var result = new Array(n),
-      len = arr.length,
-      taken = new Array(len);
-    if (n > len)
-      throw new RangeError("getRandom: more elements taken than available");
-    while (n--) {
-      var x = Math.floor(Math.random() * len);
-      result[n] = arr[x in taken ? taken[x] : x];
-      taken[x] = --len in taken ? taken[len] : len;
+  const displayCrossWord = () => {
+
+  };
+
+  const generateCrossWord = () => {
+    generateGrids();
+    obtainClues();
+    displayCrossWord();
+  };
+
+  const wordsNClues = () => {
+    word.text = getWord();
+    word.vertical = Math.random()>=0.5;
+  };
+
+  const getWord = () => {
+    let word = getRandomWord();
+
+    while(!hasStartingLetter(word)){
+      word = getRandomWord();
     }
-    return result;
+
+    return word;
   };
 
-  
+  const hasStartingLetter = (word) => {
+    let startLetter = false;
+    for(let letter of firstLetters){
+      if(letter === word.charAt(0)){
+        startLetter = true;
+        break;
+      }
+    }
+    return startLetter;
+  };
 
-  if (props.words !== undefined) {
-    let totalWords = props.words;
-    let totalClues = props.clues;
+  const getUnusedWords = () => {
+    return wordSet.filter((eachWord) => !usedWords.includes(eachWord));
+  };
 
-    wordsNClues(totalWords, totalClues);
+  const getRandomWord = () => {
+    let unusedWords = getUnusedWords();
+    return unusedWords[Math.floor(Math.random() * unusedWords.length)];
+  };
+
+  if (typeof props.words !== 'undefined') {
+    wordSet = props.words;
+    clueSet = props.clues;
+    generateCrossWord();
+    //wordsNClues();
   }
 
   const gridSlots = () => {
-    let row = 1;
-    let column = 1;
+    let row = 0;
+    let column = 0;
     let slots = [];
-    for (row = 1; row <= 10; row++) {
-      for (column = 1; column <= 10; column++) {
+    for (row = 0; row < 10; row++) {
+      for (column = 0; column < 10; column++) {
         slots.push(
           <input
             type="text"
@@ -74,9 +128,9 @@ const Grid = (props) => {
   };
 
   let content = reqClues;
-
+  
   if(props.error){
-    content = <button type="button" className="btn btn-outline-dark mt-4" onClick={props.onFetch}>
+    content = <button type="button" className="btn btn-outline-dark mt-4 tryAgnBtn" onClick={props.onFetch}>
     Try Again
   </button>
   }
@@ -84,6 +138,7 @@ const Grid = (props) => {
   if(props.loading){
     content = 'Loading Clues...';
   }
+  
   return (
     <>
       <div className="grid">{gridSlots()}</div>
