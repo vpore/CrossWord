95073481637788ed56d039c596b0a6a49fb77517
Vaibhavi <porevaibhavi5@gmail.com>
2022-03-07 19:16:21 +0530

    :sparkles: Added GitHub repo link

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,10 @@
+import GithubCorner from 'react-github-corner';
+
 const Header = () => {
     return(
         <>
             <h3 className="text-center fw-bolder mt-4">Crossword Puzzle for CODERS!</h3>
-            
+            <GithubCorner href="https://github.com/vpore" target="_blank"/>
         </>
     );
 }
